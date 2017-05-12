7a2a013e4605f09cf34086ffe82b2f01ba087c91
Andrew Rocchi <arocchi@cmu.edu>
2017-05-12 17:34:06 -0400

    Excess code trimming

--- Game.js
+++ Game.js
@@ -5,8 +5,6 @@ UndertaleBattleSimulator.Game = function (game) {
     this.secondsElapsed;
     this.timer;
     this.music;
-    this.ouch;
-    this.boom;
     this.ding;
     this.sprite;
     this.upKey;
@@ -30,8 +28,6 @@ UndertaleBattleSimulator.Game.prototype = {
         this.secondsElapsed = 0;
         this.timer = this.time.create(false);
         this.timer.loop(1000, this.updateSeconds, this);
-        /*this.totalBunnies = 20;
-        this.totalSpacerocks = 13;*/
 
         this.score = 0;
 
@@ -62,7 +58,6 @@ UndertaleBattleSimulator.Game.prototype = {
         rightKeyD = this.input.keyboard.addKey(Phaser.Keyboard.D);
         restartKey = this.input.keyboard.addKey(Phaser.Keyboard.R);
 
-
         this.music = this.add.audio('game_audio');
         this.music.play('', 0, 0.3, true);   //marker, position, volume, loop
         this.ouch = this.add.audio('hurt_audio');
@@ -76,132 +71,9 @@ UndertaleBattleSimulator.Game.prototype = {
     },
 
     buildWorld: function () {
-        /*this.add.image(0, 0, 'sky');
-        this.add.image(0, 800, 'hill');
-        this.buildBunnies();
-        this.buildSpaceRocks();
-        this.buildEmitter();*/
         this.counter = this.add.bitmapText(10, 10, 'eightbitwonder', 'Score: ' + this.score, 0);
         this.timer.start();
     },
-
-    buildBunnies: function () {
-        this.bunnygroup = this.add.group();
-        this.bunnygroup.enableBody = true;
-        for (var i=0; i<this.totalBunnies; i++) {
-            var b = this.bunnygroup.create(this.rnd.integerInRange(-10, this.world.width-50), this.rnd.integerInRange(this.world.height-180, this.world.height-60), 'heart', 'Bunny0000');
-            b.anchor.setTo(0.5, 0.5); //center point for bunnies
-            b.body.moves = false;
-            //b.animations.add('Rest', this.game.math.numberArray(1,58));
-            //b.animations.add('Walk', this.game.math.numberArray(68,107));
-            //b.animations.play('Rest', 24, true);
-            this.assignBunnyMovement(b);
-        }
-    },
-
-    /*assignBunnyMovement: function (b) {
-        bposition = Math.floor(this.rnd.realInRange(50, this.world.width-50));
-        bdelay = this.rnd.integerInRange(2000, 6000);
-        if(bposition < b.x){
-            b.scale.x = 1;
-        }else{
-            b.scale.x = -1;
-        }
-        t = this.add.tween(b).to({x:bposition}, 3500, Phaser.Easing.Quadratic.InOut, true, bdelay); //properties, duration, ease, autoStart, delay
-        t.onStart.add(this.startBunny, this);
-        t.onComplete.add(this.stopBunny, this);
-    },
-
-    startBunny: function (b) {
-        //b.animations.stop('Rest');
-        //b.animations.play('Walk', 24, true);
-    },
-
-    stopBunny: function (b) {
-        //b.animations.stop('Walk');
-        //b.animations.play('Rest', 24, true);
-        this.assignBunnyMovement(b);
-    },
-
-    buildSpaceRocks: function() {
-        this.spacerockgroup = this.add.group();
-        for(var i=0; i<this.totalSpacerocks; i++) {
-            var r = this.spacerockgroup.create(this.rnd.integerInRange(0, this.world.width), this.rnd.realInRange(-1500, 0), 'spacerock', 'SpaceRock0000');
-            var scale = this.rnd.realInRange(0.3, 1.0);
-            r.scale.x = scale;
-            r.scale.y = scale;
-            this.physics.enable(r, Phaser.Physics.ARCADE);
-            r.enableBody = true;
-            r.body.velocity.y = this.rnd.integerInRange(200, 400);
-            r.animations.add('Fall');
-            r.animations.play('Fall', 24, true);
-            r.checkWorldBounds = true;
-            r.events.onOutOfBounds.add(this.resetRock, this);
-        }
-    },
-
-    resetRock: function(r) {
-        if(r.y > this.world.height){
-            this.respawnRock(r);
-        }
-    },
-
-    respawnRock: function(r) {
-        if(this.gameover == false){
-            r.reset(this.rnd.integerInRange(0, this.world.width), this.rnd.realInRange(-1500, 0));
-            r.body.velocity.y = this.rnd.integerInRange(200, 400);
-        }
-    },
-
-    buildEmitter:function() {
-        this.burst = this.add.emitter(0, 0, 80);
-        this.burst.minParticleScale = 0.3;
-        this.burst.maxParticleScale = 1.2;
-        this.burst.minParticleSpeed.setTo(-30, 30);
-        this.burst.maxParticleSpeed.setTo(30, -30);
-        this.burst.makeParticles('explosion');
-        this.input.onDown.add(this.fireBurst, this);
-    },
-
-    fireBurst: function(pointer) {
-        if(this.gameover == false){
-            this.boom.play();
-            this.boom.volume = 0.2;
-            this.burst.emitX = pointer.x;
-            this.burst.emitY = pointer.y;
-            this.burst.start(true, 2000, null, 20); //(explode, lifespan, frequency, quantity)
-        }
-    },
-
-    burstCollision : function(r, b){
-        this.respawnRock(r);
-    },
-
-    bunnyCollision : function(r, b){
-        if(b.exists){
-            this.ouch.play();
-            this.makeGhost(b);
-            this.respawnRock(r);
-            b.kill();
-            this.totalBunnies--;
-            this.checkBunniesLeft();
-        }
-    },
-
-    checkBunniesLeft : function(){
-        if(this.totalBunnies <= 0){
-            this.gameover = true;
-            this.music.stop();
-            this.countdown.setText('Bunnies Left 0');
-            this.overmessage = this.add.bitmapText(this.world.centerX-180, this.world.centerY-40, 'eightbitwonder', 'GAME OVER\n\n' + this.secondsElapsed, 42);
-            this.overmessage.align = "center";
-            this.overmessage.inputEnabled = true;
-            this.overmessage.events.onInputDown.addOnce(this.quitGame, this);
-        }
-        else{
-            this.countdown.setText('Bunnies Left ' + this.totalBunnies);
-        }
-    },*/
 
     quitGame: function(pointer){
         $.get("transmitter.php", {score: this.score},
@@ -213,16 +85,6 @@ UndertaleBattleSimulator.Game.prototype = {
             this.state.start(this.state.current);
         }
     },
-
-    /*makeGhost: function(b) {
-        bunnyghost = this.add.sprite(b.x-20, b.y-180, 'ghost');
-        bunnyghost.anchor.setTo(0.5, 0.5);
-        bunnyghost.scale.x = b.scale.x;
-        this.physics.enable(bunnyghost, Phaser.Physics.ARCADE);
-        bunnyghost.enableBody = true;
-        bunnyghost.checkWorldBounds = true;
-        bunnyghost.body.velocity.y = -800;
-    },*/
 
     movement: function(){
         if (upKey.isDown || upKeyW.isDown){
@@ -259,23 +121,18 @@ UndertaleBattleSimulator.Game.prototype = {
     enemyHitsPlayer: function(){
         this.sprite.kill();
         this.gameover = true;
-        //this.bullets.kill();
         this.timer.pause();
         this.music.stop();
-        this.overmessage = this.add.bitmapText(this.world.centerX-180, this.world.centerY-40, 'eightbitwonder', 'GAME OVER\n\n' + 'Score:  ' + this.score, 42);
+        this.overmessage = this.add.bitmapText(this.world.centerX-300, this.world.centerY-150, 'eightbitwonder', 'GAME OVER\n\n' + 'Score:  ' + this.score + '\n\n (Hit r to restart)', 42);
         this.overmessage.align = "center";
-        this.overmessage.inputEnabled = true;
-        this.overmessage.events.onInputDown.addOnce(this.quitGame, this);     
     },
 
     fireBullet: function(){
-
         //To avoid them being allowed to fire too fast we set a time limit
         if (this.game.time.now > this.bulletTime)
         {
             //Grab the first bullet we can from the pool
             this.bullet = this.bullets.getFirstExists(false);
-
             if (this.bullet){
                 //And fire it
                 this.tooClose = true;
@@ -295,17 +152,16 @@ UndertaleBattleSimulator.Game.prototype = {
     },
 
     directionUpdate: function(bullt) {
-
         // Calculate direction towards player
         this.toPlayerX = this.sprite.x - bullt.body.x;
         this.toPlayerY = this.sprite.y - bullt.body.y;
-
         // Move towards the player
         bullt.body.velocity.x = this.toPlayerX;
         bullt.body.velocity.y = this.toPlayerY;
-
         // Rotate us to face the player
-        bullt.rotation = Math.atan2(this.toPlayerY, this.toPlayerX);
+        //bullt.rotation = Math.atan2(this.toPlayerY, this.toPlayerX);
+        bullt.angle=0;//doesn't seem to work
+        //console.log('running');
     },
 
     updateSeconds: function(){
@@ -322,17 +178,13 @@ UndertaleBattleSimulator.Game.prototype = {
     },
 
     update: function() {
-        //this.physics.arcade.overlap(this.spacerockgroup, this.burst, this.burstCollision, null, this);
-        //this.physics.arcade.overlap(this.spacerockgroup, this.bunnygroup, this.bunnyCollision, null, this);
-        //this.physics.arcade.overlap(this.bunnygroup, this.burst, this.friendlyFire, null, this);
         if(this.gameover == false){
             this.fireBullet();
             this.physics.arcade.overlap(this.sprite, this.bullets, this.enemyHitsPlayer, null, this);
-            //this.physics.arcade.overlap(this.bullets, this.bullets, this.enemyHitsPlayer, null, this);
-            //this.physics.arcade.collide(this.sprite, this.bullets);
             this.movement();
             this.boundsCollision();
             this.killBullets();
+            this.bullets.addAll('angle', 80);//doesn't seem to work at all
         }
         else{
             this.quitGame();
